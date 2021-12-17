f882cd89b62c5e9dbfcd4ab9e5cdb3ba006f1364
Alexey Orlenko <alex@aqrln.net>
2021-12-17 13:13:33 +0200

    chore: switch ts-jest/swc

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,43 @@
 "use strict";
 
-module.exports = {
-  displayName: "test",
-  preset: "ts-jest",
-  testEnvironment: "node",
-  verbose: false,
-  collectCoverage: false,
-  silent: true,
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
+const configs = {
+  "ts-jest": {
+    displayName: "test",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    verbose: false,
+    collectCoverage: false,
+    silent: true,
+    globals: {
+      "ts-jest": {
+        tsconfig: "tsconfig.json",
+      },
+    },
+  },
+
+  "@swc/jest": {
+    displayName: "test",
+    testEnvironment: "node",
+    verbose: false,
+    collectCoverage: false,
+    silent: true,
+    transform: {
+      "\\.ts$": [
+        "@swc/jest",
+        {
+          jsc: {
+            parser: {
+              syntax: "typescript",
+            },
+            target: "es2020",
+            keepClassNames: true,
+          },
+        },
+      ],
     },
   },
 };
+
+const transformer = process.env.JEST_TRANSFORMER || "@swc/jest";
+
+module.exports = configs[transformer];
